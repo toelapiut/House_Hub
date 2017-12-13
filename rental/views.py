3b2fc946f3bcf2204476f794516acc5cc6af35a5
toelapiut <toelapiut7@gmail.com>
2017-12-13 11:31:40 +0300

    added view functions to choice and landlord pages

--- rental/views.py
+++ rental/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-
+from .forms import *
 # Create your views here.
 def index(request):
 
     title='Welcome|House Hub'
 
     return render(request,'index.html',{'title':title})
+
+def choice(request):
+
+    title='LandLord|Tenant'
+
+    return render(request,'index.html',{'title':title})
+
+
+
+def landlord_prof(request):
+
+    title='username'
+
+    form=HouseForm()
+
+
+    return render(request,'landlord/profile.html',{'title':title,'form':form})
