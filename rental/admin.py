fa78db0f255cbd70d21795d8ea4ace22c1285618
toelapiut <toelapiut7@gmail.com>
2017-12-13 11:30:14 +0300

    added models to admin

--- rental/admin.py
+++ rental/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+# Register your models here.
+admin.site.register(House)
+admin.site.register(Landlord_tenant)
+admin.site.register(Profile)
+admin.site.register(Tenant)
