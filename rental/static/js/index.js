25f85c315bb7ea0325d9c1911f6cdedcd121e080
toelapiut <toelapiut7@gmail.com>
2017-12-13 22:24:01 +0300

    added functionality on on navbar

--- rental/static/js/index.js
+++ rental/static/js/index.js
@@ -5,3 +5,15 @@ $(".left a.open").on("click", function(e) {
     // get the <body>. Does it currently have a class of "open-nav"? If it does, remove the class, which will collapse the left column.' If it does not have a class of "open-nav", add it, which will expand the left column.
     $("body").toggleClass("open-nav");
   });
+
+  $(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 50) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
